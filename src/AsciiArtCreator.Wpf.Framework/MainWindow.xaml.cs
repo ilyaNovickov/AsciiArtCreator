66cb9437fead15455250259f8e086f981c435bb2
Ilya Novikov <ilyanovickov@mail.ru>
2024-02-14 20:19:02 +0300

    Первый тест MVVM паттерна

--- src/AsciiArtCreator.Wpf.Framework/MainWindow.xaml.cs
+++ src/AsciiArtCreator.Wpf.Framework/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AsciiArtCreator.Wpf.Framework.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace AsciiArtCreator.Wpf.Framework
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainViewModel();
         }
     }
 }
---
+++ src/AsciiArtCreator.Wpf.Framework/ViewModel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using AsciiArtCreator.Wpf.Framework.Commands;
+
+namespace AsciiArtCreator.Wpf.Framework.ViewModel
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private string imagePath = null;
+        private RelayCommand selectFileCommand;
+
+        public string ImagePath
+        {
+            get => imagePath;
+            set
+            {
+                if (!File.Exists(value))
+                    return;
+
+                imagePath = value;
+                OnPropertyChanged("ImagePath");
+            }
+        }
+
+        public RelayCommand SelectFileCommand
+        {
+            get => selectFileCommand ?? (selectFileCommand = new RelayCommand((_) =>
+            {
+                Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+
+                openFileDialog.Filter = "Image files (*.png, *.jpeg, *.jpg, *.bmp)|*.png;*.jpeg;*.jpg;*.bmp";
+
+                bool? ok = openFileDialog.ShowDialog();
+
+                if (ok.HasValue && ok.Value)
+                {
+                    ImagePath = openFileDialog.FileName;
+                }
+            }));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        { 
+           PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
