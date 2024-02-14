ec3827f1b7b3e8ae18fe03b660ce8a8ec6485263
Ilya Novikov <ilyanovickov@mail.ru>
2024-02-14 21:50:00 +0300

    Тесты

--- src/AsciiArtCreator.ConsolForTests/Program.cs
+++ src/AsciiArtCreator.ConsolForTests/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AsciiArtCreator.SystemDrawing.Framework;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,21 @@ namespace AsciiArtCreator.ConsolForTests
 {
     internal class Program
     {
+        const string path = @"C:\Users\ACER\Pictures\winxp.png";
+
         static void Main(string[] args)
         {
+            GrayscaleAsciiArt art = new GrayscaleAsciiArt();
+
+            art.SetImage(path);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            art.GetOrCreateAsciiArt();
+            sw.Stop();
+
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            Console.ReadLine();
         }
     }
 }
--- src/AsciiArtCreator.SystemDrawing.Framework/GrayscaleAsciiArt.cs
+++ src/AsciiArtCreator.SystemDrawing.Framework/GrayscaleAsciiArt.cs
@@ -4,6 +4,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace AsciiArtCreator.SystemDrawing.Framework
 {
@@ -72,8 +74,8 @@ namespace AsciiArtCreator.SystemDrawing.Framework
             public bool InverseColors { get; set; }
         }
 
-        private Image image;
-        private string art;
+        private Image image = null;
+        private string art = null;
         private GrayscaleArtOptions options = new GrayscaleArtOptions();
 
         public GrayscaleAsciiArt()
@@ -105,14 +107,67 @@ namespace AsciiArtCreator.SystemDrawing.Framework
 
         public event EventHandler ImageChanged;
 
+        public bool SetImage(string path)
+        {
+            if (!File.Exists(path))
+                return false;
+
+            try
+            {
+                Image = Image.FromFile(path);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            
+        }
+
         public string GetAsciiArt()
         {
             return art;
         }
 
-        public void GetOrCreateAsciiArt()
-        {
+        //public void GetOrCreateAsciiArt()
+        //{
+        //    Bitmap image = new Bitmap(this.image.Width, this.image.Height);
 
-        }
+        //    var g = Graphics.FromImage(image);
+
+        //    var b = 0;
+        //    var c = 1f;
+        //    var t = (1f - c) / 2f;
+        //    var s = 0;
+        //    var sr = (1 - s) * 0.3086f;
+        //    var sg = (1 - s) * 0.6094f;
+        //    var sb = (1 - s) * 0.0820f;
+
+        //    float[][] matrix = {
+        //       new float[] {c*(sr+s), c*sr,     c*(sr),    0, 0},
+        //       new float[] {c*sg,     c*(sg+s), c*(sg),    0, 0},
+        //       new float[] {c*sb,     c*sb,     c*(sb+s),  0, 0},
+        //       new float[] {0,        0,        0,        1f, 0},
+        //       new float[] {t+b,      t+b,      t+b,       0, 1}
+        //       //new float[] {c*(sr+s), 0,     0,    0, 0},
+        //       //new float[] {0,     c*(sg+s), 0,    0, 0},
+        //       //new float[] {0,     0,     c*(sb+s),  0, 0},
+        //       //new float[] {0,        0,        0,        1f, 0},
+        //       //new float[] {t+b,      t+b,      t+b,       0, 1}
+        //    };
+
+
+        //    ImageAttributes attributes = new ImageAttributes();
+
+        //    attributes.SetColorMatrix(new ColorMatrix(matrix));
+
+        //    g.DrawImage(this.image, new Rectangle(0,0,image.Width,image.Height), 
+        //        0, 0, this.image.Width, this.image.Height, GraphicsUnit.Pixel, attributes);
+
+        //    g.Dispose();
+
+        //    image.Save("test.png");
+
+        //}
     }
 }
